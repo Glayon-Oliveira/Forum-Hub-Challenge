af9ef026003599e29f1733fd139a50d8f9491e62
Solansra <solansra@gmail.com>
2024-12-18 22:36:29 -0300

    Implementar segurança basica

---
+++ src/main/java/com/lmlasmo/forum/infra/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.lmlasmo.forum.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		return http.csrf(c -> c.disable())
+				   .formLogin(f -> f.disable())
+				   .httpBasic(Customizer.withDefaults())
+				   .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				   .authorizeHttpRequests(a -> a.requestMatchers("/login").permitAll()
+						   .anyRequest().authenticated())
+				   .build();		
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConf) throws Exception {
+		return authenticationConf.getAuthenticationManager();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
